bf89ba7df6175546aec48a49d24b76c8ce252c1c
Gaurav Sehgal <gsehgal1@gmail.com>
2025-12-19 21:36:55 +0530

    Replace test output with 'KKKKKKKKKK'

--- Test.java
+++ Test.java
@@ -4,6 +4,6 @@ public class  Test{
 
 	public static void main(String[] args){
 		System.out.println("OK THIS IS A TEST");
-		System.out.println("OK......");
+		System.out.println("KKKKKKKKKK");
 	}
 }
